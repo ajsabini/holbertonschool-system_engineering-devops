8eda41a544ae4653321b00567774037d13b869a8
Alvaro <ajsabini@gmail.com>
2022-09-06 16:40:28 -0700

    0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""sahda"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    #print(len(argv))
+    if len(argv) > 1:
+        if argv[1].isnumeric():
+            idUsuario = argv[1]
+            #print(idUsuario)
+            response = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                                    .format(idUsuario))
+            if response.status_code == 200:
+                nombre = response.json().get('name')
+                todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(idUsuario))
+                tasks = len(todos.json())
+                completas = 0
+                for task in todos.json():
+                    if task.get('completed') is True:
+                        completas+=1
+                        titulo = task.get('title')
+                print("Employee {} is done with tasks({}/{})".format(nombre, completas, tasks))
+                print("\t {}".format(titulo))
+            else:
+                print("Hubo un error, Codigo {}".format(response.status_code))
+        else:
+            print("El argumneto debe ser un numero")
+    #nombre = usuario.json().get('name')
+    else:
+        print("Falta el argumento")
+
+
+
