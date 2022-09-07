19e0be6eaa35b5bff9ce795e42bec8400a19d1ef
Alvaro <ajsabini@gmail.com>
2022-09-07 16:55:16 -0700

    2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""export api to csv"""
+import json
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    if len(argv) > 1:
+        if argv[1].isnumeric():
+            iu = argv[1]
+            response = requests.get(
+                    'https://jsonplaceholder.typicode.com/users/{}'.format(
+                        iu))
+            if response.status_code == 200:
+                no = response.json().get('name')
+                urlTs = 'https://jsonplaceholder.typicode.com/todos?userId={}'
+                response_todos = requests.get(urlTs.format(iu))
+                tasks = len(response_todos.json())
+                todos = response_todos.json()
+
+                with open("{}".format(iu) + ".json", "w", newline='') as f:
+                    obj = []
+                    for tk in todos:
+                        obj.append({'task': tk.get('title'),
+                                    'completed': tk.get('completed'),
+                                    'username': response.json()
+                                                .get('username')})
+                    fileObj = {'{}'.format(iu): obj}
+                    json.dump(fileObj, f)
+            else:
+                print("Hubo un error, Codigo {}".format(response.status_code))
+        else:
+            print("El argumneto debe ser un numero")
+    else:
+        print("Falta el argumento")
