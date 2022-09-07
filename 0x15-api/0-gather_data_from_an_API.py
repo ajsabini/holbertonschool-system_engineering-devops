cd6a9889adf96e17f2669b488015627f82f440c6
Alvaro <ajsabini@gmail.com>
2022-09-07 13:55:18 -0700

    0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,21 +9,24 @@ if __name__ == "__main__":
     if len(argv) > 1:
         if argv[1].isnumeric():
             idUsuario = argv[1]
-            response = requests.get("https://jsonplaceholder.typicode.com\
-                                   /users/{}".format(idUsuario))
+            response = requests.get(
+                    'https://jsonplaceholder.typicode.com/users/{}'.format(
+                        idUsuario))
             if response.status_code == 200:
                 nombre = response.json().get('name')
-                todos = requests.get('https://jsonplaceholder.typicode.com\
-                                      /todos?userId={}'.format(idUsuario))
-                tasks = len(todos.json())
+                urlTs = 'https://jsonplaceholder.typicode.com/todos?userId={}'
+                response_todos = requests.get(urlTs.format(idUsuario))
+                tasks = len(response_todos.json())
+                todos = response_todos.json()
                 completas = 0
-                for task in todos.json():
+                for task in todos:
                     if task.get('completed') is True:
                         completas += 1
-                        titulo = task.get('title')
                 print("Employee {} is done with tasks({}/{})".
                       format(nombre, completas, tasks))
-                print("\t {}".format(titulo))
+                for compl in todos:
+                    if compl.get('completed') is True:
+                        print("\t {}".format(compl.get('title')))
             else:
                 print("Hubo un error, Codigo {}".format(response.status_code))
         else:
