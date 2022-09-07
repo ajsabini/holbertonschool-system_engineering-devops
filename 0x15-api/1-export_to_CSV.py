0256fee20f2458f94698991481ef9076f2f42634
Alvaro <ajsabini@gmail.com>
2022-09-07 16:06:59 -0700

    1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,25 +8,23 @@ if __name__ == "__main__":
 
     if len(argv) > 1:
         if argv[1].isnumeric():
-            idUsuario = argv[1]
+            iu = argv[1]
             response = requests.get(
                     'https://jsonplaceholder.typicode.com/users/{}'.format(
                         idUsuario))
             if response.status_code == 200:
-                nombre = response.json().get('name')
+                no = response.json().get('name')
                 urlTs = 'https://jsonplaceholder.typicode.com/todos?userId={}'
                 response_todos = requests.get(urlTs.format(idUsuario))
                 tasks = len(response_todos.json())
                 todos = response_todos.json()
 
-
                 with open("{}".format(idUsuario) + ".csv", "w") as file:
                     writing = csv.writer(file, dialect='unix')
-                    for task in todos:
-                        writing.writerow([idUsuario,
-                                            nombre,
-                                            task.get('completed'),
-                                            task.get('title')])
+                    for tk in todos:
+                        writing.writerow(
+                                [iu, no, tk.get('completed'), tk.get('title')]
+                                )
             else:
                 print("Hubo un error, Codigo {}".format(response.status_code))
         else:
