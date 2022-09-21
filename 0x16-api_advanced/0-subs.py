78b26474a499b274dbd76c87a8368e97eef2a18a
ajsabini <ajsabini@gmail.com>
2022-09-21 23:47:08 +0000

    fix PEP8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,19 @@
-#!/usr/bin/python3
+#!usr/bin/python3
 """task 0"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-	""" returns the number of subscribers (not active users, total subscribers) for a given subreddit  """
+    """ returns the number of subscribers (not active users,
+    total subscribers) for a given subreddit  """
 
-	subs_count = requests.get("https://www.reddit.com/r/{}/about.json"
-                                  .format(subreddit),headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"},allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
+                AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0\
+                Safari/537.36"}
+    subs_count = requests.get(url, headers=headers, allow_redirects=False)
 
-	if subs_count.status_code == 200:
-		return subs_count.json()["data"]["subscribers"]
-	else:
-		return 0
+    if subs_count.status_code == 200:
+        return subs_count.json()["data"]["subscribers"]
+    else:
+        return 0
