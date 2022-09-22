bc56251b5426a7d4034257ebe0ea46fd36636a69
ajsabini <ajsabini@gmail.com>
2022-09-22 20:52:13 +0000

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,7 +3,6 @@ exec { 'fixing':
   command  => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   provider => shell,
 }
-
 exec { 'apache restart':
   command  => '/etc/init.d/apache2 restart',  
   provider => shell,
