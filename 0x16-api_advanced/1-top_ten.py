80e077941146c67d31f0d0485aa64463c5829813
ajsabini <ajsabini@gmail.com>
2022-09-22 00:31:04 +0000

    f p 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""task 0"""
+"""task 1"""
 import requests
 
 
 def top_ten(subreddit):
-	""" returns the number of subscribers (not active users, total subscribers) for a given subreddit  """
+    """ print title of 10 hot posts """
 
-	subs_hot = requests.get("https://www.reddit.com/r/{}/hot.json"
-                                  .format(subreddit),headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"},allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    hedaers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
+                (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
 
-	if subs_hot.status_code == 200:
-		data = subs_hot.json()
-		hot_posts = data["data"]["children"]
-		for post in hot_posts[:10]:
-			print(post["data"]["title"])
-	else:
-		print("None")
+    subs_hot = requests.get(url, headers=headers, allow_redirects=False)
+
+    if subs_hot.status_code == 200:
+        data = subs_hot.json()
+        hot_posts = data["data"]["children"]
+        for post in hot_posts[:10]:
+            print(post["data"]["title"])
+    else:
+        print("None")
