bba4682679150819c0336b9b0180ac794d8d3126
ajsabini <ajsabini@gmail.com>
2022-09-21 22:13:46 -0300

    f 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,17 +7,16 @@ def top_ten(subreddit):
     """ print title of 10 hot posts """
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36\
-                (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleW\
+            ebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
 
     subs_hot = requests.get(url, headers=headers)
 
     if subs_hot.status_code < 300:
         data = subs_hot.json()
+        hot_posts = data['data']['children']
+        for post in hot_posts[:10]:
+            print(post['data']['title'])
 
-        for i in range(len(data['data']['children'])):
-            print(data['data']['children'][i]['data']['title'])
-            if i == 9:
-                break
     else:
         print("None")
