ca63816cdc630633711903a547bd4cb76baac401
ajsabini <ajsabini@gmail.com>
2022-09-22 21:22:45 +0000

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,9 @@
-#fixing apache bug
-exec {'fixing bug':
+#search and replace bad name
+exec {'web debugging':
   command  => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
   provider => shell,
 }
-exec {'apache restart':
-  command  => '/etc/init.d/apache2 restart',  
+exec {'apache start':
+  command  => '/etc/init.d/apache2 restart',
   provider => shell,
 }
