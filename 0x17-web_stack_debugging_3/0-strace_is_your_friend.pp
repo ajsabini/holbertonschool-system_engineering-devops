ce815bbca3c80bbbf48591df8cea5477427372cf
ajsabini <ajsabini@gmail.com>
2022-09-22 21:27:01 +0000

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #search and replace bad name
 exec {'web debugging':
-  command  => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+  command  => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   provider => shell,
 }
 exec {'apache start':
