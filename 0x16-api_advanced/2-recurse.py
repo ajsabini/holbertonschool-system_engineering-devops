ecf7b09ccca2f14d1d6930806b985a2f935b6b50
ajsabini <ajsabini@gmail.com>
2022-09-21 22:20:56 -0300

    f 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,31 +4,30 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after="inicio"):
-	""" returns the number of subscribers (not active users,
+    """ returns the number of subscribers (not active users,
     total subscribers) for a given subreddit  """
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headersUA = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
     AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"}
 
-	if after != "inicio":
-		url = url + "?after={}".format(after)
-		
-	
-	subs_hot = requests.get(url,headers=headersUA,allow_redirects=False)
-	data = subs_hot.json()
-	
-	hotTitles = data.get("data", {}).get("children", [])
+    if after != "inicio":
+        url = url + "?after={}".format(after)
 
-	if not hotTitles:
-		return None
+    subs_hot = requests.get(url, headers=headersUA, allow_redirects=False)
+    data = subs_hot.json()
 
-	for ht in hotTitles:
-		hot_list.append(ht["data"]["title"])
+    hotTitles = data.get("data", {}).get("children", [])
 
-	after = data["data"]["after"]
+    if not hotTitles:
+        return None
 
-	if not after:
-		return hot_list
+    for ht in hotTitles:
+        hot_list.append(ht["data"]["title"])
 
-	return (recurse(subreddit, hot_list, after))
+    after = data["data"]["after"]
+
+    if not after:
+        return hot_list
+
+    return (recurse(subreddit, hot_list, after))
