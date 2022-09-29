a63e52f2a7337a7e346bfa1bb741d761b83460eb
ajsabini <ajsabini@gmail.com>
2022-09-29 13:30:00 +0000

    f0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # setting nginx without systemctl
-exec { 'replace value':
-  	command  => "sed -i 's/-n 15/-n 4096/g' /etc/default/nginx",
-	provider => shell
+exec {'replace value':
+        command  => "sed -i 's/-n 15/-n 4096/g' /etc/default/nginx",
+        provider => shell
 }
-exec { 'restart nginx':
-  	command  => '/etc/init.d/nginx restart',
-	provider => shell
+exec {'restart nginx':
+        command  => '/etc/init.d/nginx restart',
+        provider => shell
 }
